e65244458ddc3175c0e25053f1503e343cc6df57
Poostaq <piootrek.nowicki@gmail.com>
2017-03-18 15:34:32 +0100

    Chyba juz dziala

--- src/com/poostaq/Main.java
+++ src/com/poostaq/Main.java
@@ -1,14 +1,27 @@
 package com.poostaq;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.lang.String;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        List<String> listaPierwsza = Arrays.asList("aaa", "bbb", "ccc", "xxx");
+        List<String> listaDruga = Arrays.asList("bbb","ccc","ddd", "yyy");
+        compareLists(listaPierwsza, listaDruga);
     }
 
-    public ArrayList compareLists(ArrayList list1, ArrayList list2){
-        
+    private static void compareLists(List<String> list1, List<String> list2){
+        String list2missing = list1.stream()
+                .filter(s -> !list2.contains(s))
+                .collect(Collectors.joining(", "));
+        String list1missing = list2.stream()
+                .filter(s -> !list1.contains(s))
+                .collect(Collectors.joining(", "));
+        System.out.format("W liscie 1 brakuje wartosci %s z listy 2, a w liscie 2 brakuje wartosci %s z listy 1", list1missing, list2missing);
     }
 }
